98626ebc01140b173d335dc189c5d69b0c7180df
Serhiy Boychenko <serhiy.boychenko@cern.ch>
2019-11-22 17:37:49 +0100

    Simplify the application

---
+++ src/main/java/xyz/codingdaddy/Application.java
@@ -0,0 +1,18 @@
+package xyz.codingdaddy;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class Application {
+    void onStart(@Observes StartupEvent startupEvent) {
+        System.out.println("The application is starting . . .");
+    }
+
+    void onStop(@Observes ShutdownEvent shutdownEvent) {
+        System.out.println("The application is stopping . . .");
+    }
+}
